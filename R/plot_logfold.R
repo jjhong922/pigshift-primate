982be8a9a476fc5ec0da1cffb59a17ad13f5b2f3
Justin Hong <jjhong922@berkeley.edu>
2016-07-16 15:22:07 -0700

    made filepath generic and fixed print commands

--- R/plot_logfold.R
+++ R/plot_logfold.R
@@ -1,23 +1,23 @@
-library(PIGShift, ape)
+library(PIGShift)
+library(ape)
 
-groups <- read.groups("/Users/jhong/Documents/Berkeley/Research/pigshift-primate/data/go/human/human-go-terms.txt")
-primate.tree = read.tree("/Users/jhong/Documents/Berkeley/Research/pigshift-primate/data/misc/brawand-et-al-primate-tree.tre");
+groups <- read.groups("../data/go/human/human-go-terms.txt")
+primate.tree = read.tree("../data/misc/brawand-et-al-primate-tree.tre")
 
 for (name in c("female-br", "female-cb", "female-ht", "female-kd", "male-br", "male-ht", "male-kd", "male-lv")) {
-	data <- read.exp(sprintf("/Users/jhong/Documents/Berkeley/Research/pigshift-primate/data/expression/primate/brawand-et-al/matrices/primate-%s.tsv", name), primate.tree)
-	maxfile <- read.table(sprintf("/Users/jhong/Documents/Berkeley/Research/pigshift-primate/output/primate-%s/waic_max-%s.tsv", name, name), sep="\t", header=TRUE)
+	data <- read.exp(sprintf("../data/expression/primate/brawand-et-al/matrices/primate-%s.tsv", name), primate.tree)
+	maxfile <- read.table(sprintf("../output/primate-%s/waic_max-%s.tsv", name, name), sep="\t", header=TRUE)
 	siggroups <- list()
 	for (i in 1:nrow(maxfile)) {
-		if (maxfile[i,]$max >= .98 && (maxfile[i,]$maxcol != 0 || maxfile[i,]$maxcol != 1) {
+		if (maxfile[i,]$max >= .98 && (maxfile[i,]$maxcol != 0 || maxfile[i,]$maxcol != 1)) {
 			siggroups <- c(siggroups, row.names(maxfile[i,]))
 		}
 	}
-	print(length(siggroups))
+	print(sprintf("%s plots to create for %s", length(siggroups), name))
 	for (group in siggroups) {
-		pdf(sprintf("/Users/jhong/Documents/Berkeley/Research/pigshift-primate/plots/primate-%s/%s_plot.pdf", name, group))
-		plot_logfoldchange(data, groups, group_name = group, main = sprintf("%s(V%s)", group_name, maxfile[group_name,]$maxcol))
+		pdf(sprintf("../plots/primate-%s/%s_plot.pdf", name, group))
+		print(group)
+		plot_logfoldchange(data, groups, group_name = group, main = sprintf("%s(V%s)", group, maxfile[group,]$maxcol))
 		dev.off()
 	}
 }	
-
-#figure out models
