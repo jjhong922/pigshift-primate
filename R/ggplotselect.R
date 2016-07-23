da48730d6c62bb2ab29fbb5c5cfcb06684a83219
Justin Hong <jjhong922@berkeley.edu>
2016-07-22 17:22:58 -0700

    ggplot script and plots created, plot log fold edited for fixed xlim to rerun on server

---
+++ R/ggplotselect.R
@@ -0,0 +1,33 @@
+library(PIGShift)
+library(ape)
+library(ggplot2)
+
+groups <- read.groups("../data/go/human/human-go-terms.txt")
+primate.tree <- read.tree("../data/misc/brawand-et-al-primate-tree.tre")
+sigterms <- read.table("../sigGOterms.tsv", sep = "\t")
+
+organlist <- c("female-br", "female-cb", "female-ht", "female-kd", "male-br", "male-ht", "male-kd", "male-lv")
+data <- list()
+for (i in 1:length(organlist)) {
+	data[[i]] <- read.exp(sprintf("../data/expression/primate/brawand-et-al/matrices/primate-%s.tsv", 
+		organlist[[i]]), primate.tree)
+	print(i)
+}
+
+for (i in 1:nrow(sigterms)) {
+	curdat <- data[[which(organlist == as.character(sigterms[i,]$organ))]]
+	curdat <- data.frame(t(curdat[,which(colnames(curdat)%in%groups[[as.character(sigterms[i,]$GO_terms)]])]))
+	curdat <- stack(curdat)
+	
+	print(as.character(sigterms[i,]$GO_terms))
+	
+	ggplot(curdat[curdat$ind != "human",], aes(x=values)) + 
+		geom_density(aes(group = ind, colour = ind, fill = ind), alpha = 0.3) + 
+		scale_x_continuous(limits = c(-20, 20)) +
+		ggtitle(sprintf("%s(%s)-%s", sigterms[i,]$GO_terms, sigterms[i,]$model, sigterms[i,]$organ)) +
+		theme_minimal()
+
+	ggsave(sprintf("../plots/sig_ggplots/%s-%s_plot.png", sigterms[i,]$organ, sigterms[i,]$GO_terms))
+}
+
+print("script complete.")
