ed3caefa3b1d8b2aede56b8dcbd803e8bbf3380f
Justin Hong <jjhong922@berkeley.edu>
2016-07-23 15:41:45 -0700

    adjusted filters on plot creation to those with shifted modes. created ggplots for them.

--- R/plot_gglogfold.R
+++ R/plot_gglogfold.R
@@ -5,6 +5,17 @@ library(ggplot2)
 groups <- read.groups("../data/go/human/human-go-terms.txt")
 primate.tree <- read.tree("../data/misc/brawand-et-al-primate-tree.tre")
 
+#from stackoverflow- finds dominant mode of a density plot
+dmode <- function(x, th) {
+    den <- density(x, kernel=c("gaussian"))
+    (abs(den$x[den$y == max(den$y)]) > th)   
+}  
+
+dmodey <- function(x, th) {
+	den <- density(x, kernel = c("gaussian"))
+	(max(den$y) > th)
+}
+
 for (name in c("female-br", "female-cb", "female-ht", "female-kd", "male-br", "male-ht", "male-kd", "male-lv")) {
 	data <- read.exp(sprintf("../data/expression/primate/brawand-et-al/matrices/primate-%s.tsv", name), primate.tree)
 	maxfile <- read.table(sprintf("../output/primate-%s/waic_max-%s.tsv", name, name), sep = "\t", header = TRUE)
@@ -14,19 +25,21 @@ for (name in c("female-br", "female-cb", "female-ht", "female-kd", "male-br", "m
 			siggroups <- c(siggroups, row.names(maxfile[i,]))
 		}
 	}
-	print(sprintf("%s plots to create for %s", length(siggroups), name))
+	print(sprintf("%s potential plots to create for %s", length(siggroups), name))
 
 	for (group in siggroups) {
-		print(group)
-		
-		curdata <- data.frame(t(data[,which(colnames(data)%in%groups[[group]])]))
-		curdata <- stack(curdata)
-		ggplot(curdata[curdata$ind != "human",], aes(x=values)) + 
-			geom_density(aes(group = ind, colour = ind)) + 
-			scale_x_continuous(limits = c(-20, 20)) +
-			ggtitle(sprintf("%s(%s)-%s", group, maxfile[group,]$maxcol, name)) +
-			theme_minimal()
+		curdat <- data.frame(t(data[,which(colnames(data)%in%groups[[group]])]))
+		if (any(unlist(sapply(curdat, function(x){dmode(x, 3)}))) && 
+		  any(sapply(curdat[,-1], function(x){dmodey(x, 0.05)}))){
+			curdat <- stack(curdat)
+			ggplot(curdat[curdat$ind != "human",], aes(x = values)) + 
+				geom_density(aes(group = ind, colour = ind)) + 
+				coord_cartesian(xlim = c(-20, 20)) +
+				ggtitle(sprintf("%s(%s)-%s", group, maxfile[group,]$maxcol, name)) +
+				theme_minimal()
 
-		ggsave(sprintf("../plots/primate-%s/%s_plot.pdf", name, group))
+			ggsave(sprintf("../plots/primate-%s/%s_plot.pdf", name, group))
+			print(group)
+		}
 	}
 }	
